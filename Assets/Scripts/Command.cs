61b27f09a7dac51b111a00b2d9e6dbfea25de30d
ShadowKillerZero <15579136184@163.com>
2018-07-05 09:34:38 +0800

    update command

--- Assets/Scripts/Command.cs
+++ Assets/Scripts/Command.cs
@@ -29,6 +29,58 @@ public class FireCommand : Command
     }
 }
 
+public class Unit
+{
+
+    private int x;
+    private int y;
+    public int X
+    {
+        get
+        {
+            return x;
+        }
+    }
+    public int Y
+    {
+        get
+        {
+            return y;
+        }
+    }
+
+    public void MoveTo(int x,int y)
+    {
+        this.x = x;
+        this.y = y;
+        //Move
+    }
+
+}
+
+public class MoveUnitCommand : Command
+{
+    public MoveUnitCommand(Unit unit,int x,int y)
+    {
+        this.unit = unit;
+        this.mx = x;
+        this.my = y;
+        this.xBefore = 0;
+        this.yBefore = 0;
+    }
+    public override void Execute(GameActor actor)
+    {
+        xBefore = unit.X;
+        yBefore = unit.Y;
+        unit.MoveTo(mx, my);
+    }
+
+    private int xBefore, yBefore;
+
+    private int mx, my;
+    private Unit unit;
+}
+
 public class InputHandler
 {
     public Command HandleInput()
